01d29b7d8428397197c17ded477952d62b94a0b2
EdeiltonSO <edeiltonso@gmail.com>
2018-08-30 19:32:44 -0300

    Adicionados Highlight.js e markdown

--- app/models/snippet.js
+++ app/models/snippet.js
@@ -1,9 +1,36 @@
+const hljs = require('highlight.js');
+
+const md = require('markdown-it')({
+  highlight: (str, lang) => {
+    if (lang && hljs.getLanguage(lang)) {
+      return `<pre class="hljs"><code>${hljs.highlight(lang, str.trim(), true).value}</code></pre>`;
+    }
+
+    return `<pre class="hljs"><code>${md.utils.escapeHtml(str.trim())}</code></pre>`;
+  },
+});
+
 module.exports = (sequelize, DataTypes) => {
-  const Snippet = sequelize.define('Snippet', {
-    // campos estrangeiros não ficam aqui
-    title: DataTypes.STRING,
-    content: DataTypes.TEXT,
-  });
+  const Snippet = sequelize.define(
+    'Snippet',
+    {
+      // campos estrangeiros não ficam aqui
+      title: DataTypes.STRING,
+      content: DataTypes.TEXT,
+    },
+    {
+      getterMethods: {
+        except() {
+          return this.content.length > 120
+            ? `${this.content.substring(0, this.content.lastIndexOf(' ', 120))}...`
+            : this.content;
+        },
+        formattedContent() {
+          return md.render(this.content);
+        },
+      },
+    },
+  );
 
   // É preciso informar pro model que existe
   // uma associação dele com o model de usuário.
